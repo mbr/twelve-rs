0faceedabfdf6b1b8fc50327e779b5f0b447e4d2
Marc Brinkmann <git@marcbrinkmann.de>
2024-02-05 11:47:04 +0100

    Move `tracing_subscriber` setup to newly created `twelve::setup` function

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,14 @@
 
 pub mod page;
 pub mod util;
+
+/// Perform initial setup
+///
+/// # Panics
+///
+/// Will panic if setting up the tracing subscriber fails.
+pub fn setup() {
+    tracing_subscriber::fmt()
+        .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
+        .init();
+}
