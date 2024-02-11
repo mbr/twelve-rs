34ee3cd5a85f3273287d41cfeb749df39925b09f
Marc Brinkmann <git@marcbrinkmann.de>
2024-02-11 10:30:55 +0100

    Reduce `serve` support to `Router<()>`

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,9 @@
 #![doc = include_str!("../README.md")]
 
-use std::{
-    convert::Infallible,
-    net::{IpAddr, SocketAddr},
-};
+use std::net::{IpAddr, SocketAddr};
 
-use axum::{extract::Request, response::Response, serve::IncomingStream};
+use axum::Router;
 use serde::de::DeserializeOwned;
-use tower_service::Service;
 use tracing::info;
 
 pub mod page;
@@ -26,20 +22,14 @@ pub fn setup<T: DeserializeOwned>() -> T {
     envy::from_env().expect("failed to parse configuration from environment")
 }
 
-pub async fn serve<M, S>(port: u16, make_service: M)
-where
-    M: for<'a> Service<IncomingStream<'a>, Error = Infallible, Response = S> + Send + 'static,
-    for<'a> <M as Service<IncomingStream<'a>>>::Future: Send,
-    S: Service<Request, Response = Response, Error = Infallible> + Clone + Send + 'static,
-    S::Future: Send,
-{
+pub async fn serve(port: u16, router: Router) {
     let listen_addr = SocketAddr::from((IpAddr::from([0, 0, 0, 0]), port));
     info!("listening on {}", listen_addr);
 
     let listener = tokio::net::TcpListener::bind(listen_addr)
         .await
         .expect("could not bind to port");
-    axum::serve(listener, make_service)
+    axum::serve(listener, router)
         .with_graceful_shutdown(util::graceful_shutdown::setup_and_wait_for_shutdown())
         .await
         .expect("crashed");
